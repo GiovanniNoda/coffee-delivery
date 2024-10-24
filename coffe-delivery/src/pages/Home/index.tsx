f10edfec88f0c535e4ba458df35c93b78ad0c572
Giovanni <giovanniminoru@gmail.com>
2024-10-24 03:44:36 -0300

    iniciado o home

--- coffe-delivery/src/pages/Home/index.tsx
+++ coffe-delivery/src/pages/Home/index.tsx
@@ -1,10 +1,65 @@
 import { HomeContainer } from "./styles"
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react"
+import CoffeImage from "../../assets/image.svg"
 
 export function Home() {
     return(
         <HomeContainer>
-            <h1>Home</h1>
-            
+            <div className="container">
+                <div className="title-container">
+                    <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+
+                    <div className="items-container">
+                        <div className="two-items-container">
+                            <div className="item-wrapper">
+                                <span>
+                                    <ShoppingCart size={16} />
+                                </span>
+
+                                <span>
+                                    Compra simples e segura
+                                </span>
+                            </div>
+
+                            <div className="item-wrapper">
+                                <span>
+                                    <Package size={16} />
+                                </span>
+
+                                <span>
+                                Embalagem mantém o café intacto
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="two-items-container">
+                            <div className="item-wrapper">
+                                <span>
+                                    <Timer size={16} />
+                                </span>
+
+                                <span>
+                                Entrega rápida e rastreada
+                                </span>
+                            </div>
+
+                            <div className="item-wrapper">
+                                <span>
+                                    <Coffee size={16} />
+                                </span>
+
+                                <span>
+                                O café chega fresquinho até você
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <img src={CoffeImage} alt="" />
+            </div>
         </HomeContainer>
     )
 }
--- coffe-delivery/src/pages/Home/styles.ts
+++ coffe-delivery/src/pages/Home/styles.ts
@@ -2,4 +2,10 @@ import styled from "styled-components"
 
 export const HomeContainer = styled.main`
     width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+
+    .container {
+        background-image: linear-gradient(90deg, #8047F8, #F1E9C9);
+    }
 `
