99592a766bcca47ec1e1d5dfc70f6656f4196e14
Giovanni <giovanniminoru@gmail.com>
2024-10-26 02:30:57 -0300

    Create mediaqueries.ts

---
+++ coffe-delivery/src/styles/mediaqueries.ts
@@ -0,0 +1,11 @@
+const sizes = {
+    mobile: '425',
+    tablet: '768px',
+    laptopM: '1024px',
+}
+
+export const media = {
+    mobile: `@media (max-width: ${sizes.mobile})`,
+    tablet: `@media (max-width: ${sizes.tablet})`,
+    laptopM: `@media (max-width: ${sizes.laptopM})`,
+}
